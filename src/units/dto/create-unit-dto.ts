03b708578e661ae1e3886a391585ce43ed2d96f3
UzurpatorUzbekistanu <b_kleszcz@interia.pl>
2025-11-14 07:20:35 +0100

    swagger added

--- src/units/dto/create-unit-dto.ts
+++ src/units/dto/create-unit-dto.ts
@@ -1,20 +1,33 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsInt, IsString } from "class-validator";
+import * as unitTypes from "../domain/unit.types";
 
 export class CreateUnitDto {
+    @ApiProperty()
     @IsString()
     name: string;
+    @ApiProperty()
+    @IsString()
+    id: unitTypes.UnitName;
+    @ApiProperty()
     @IsInt()
     maxHp: number;
+    @ApiProperty()
     @IsInt()
     meleeAttack: number
+    @ApiProperty()
     @IsInt()
     rangedAttack: number;
+    @ApiProperty()
     @IsInt()
     attackRange: number;
+    @ApiProperty()
     @IsInt()
     defense: number;
+    @ApiProperty()
     @IsInt()
     speed: number;
+    @ApiProperty()
     @IsInt()
     cost: number;
 }
--- src/units/units.controller.ts
+++ src/units/units.controller.ts
@@ -3,12 +3,15 @@ import { CreateUnitDto } from './dto/create-unit-dto';
 import type { UnitName } from './domain/unit.types';
 import { UnitsService } from './units.service';
 import { GameUnit } from './domain/game-unit';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller('units')
 export class UnitsController {
     constructor(private readonly unitsService: UnitsService) {}
 
     @Post('create:id')
+    @ApiOperation({ summary: 'Create a new unit by its name like: line-infantry' })
+    @ApiResponse({ status: 201, description: 'The unit has been successfully created.', type: CreateUnitDto })
     createUnit(@Param('id') id: UnitName ): CreateUnitDto {
         
         const unit: GameUnit =this.unitsService.createUnit(id);
